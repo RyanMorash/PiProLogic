c0e8c38e22db5317c1f05734f994496e8c242842
Dan Bean <shannonborho@hotmail.com>
2018-07-25 23:45:03 -0400

    remove error handler function

--- app.js
+++ app.js
@@ -25,17 +25,6 @@ raspi.init(()=> {
         next(createError(404));
     });
 
-    // error handler
-    app.use(function (err, req, res, next) {
-        // set locals, only providing error in development
-        res.locals.message = err.message;
-        res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-        // render the error page
-        res.status(err.status || 500);
-        res.render('error');
-    });
-
     let serial = new Serial({
         portId: device,
         baudRate: 19200
