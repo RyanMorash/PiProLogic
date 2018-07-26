cbba4e7e8d35f30b8337c59d59ad0f74f5195a50
Dan Bean <shannonborho@hotmail.com>
2018-07-25 23:47:00 -0400

    Bring error handler back but put it in the right location

--- app.js
+++ app.js
@@ -20,11 +20,6 @@ raspi.init(()=> {
     app.use(logger(logFormat));
     app.use(bodyParser.text({type: '*/*'}));
 
-    // catch 404 and forward to error handler
-    app.use(function (req, res, next) {
-        next(createError(404));
-    });
-
     let serial = new Serial({
         portId: device,
         baudRate: 19200
@@ -124,6 +119,22 @@ raspi.init(()=> {
                     res.send(automationDetails.poolTemperature)
                 }
             );
+        });
+
+        // catch 404 and forward to error handler
+        app.use(function (req, res, next) {
+            next(createError(404));
+        });
+
+        // error handler
+        app.use(function (err, req, res, next) {
+            // set locals, only providing error in development
+            res.locals.message = err.message;
+            res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+            // render the error page
+            res.status(err.status || 500);
+            res.render('error');
         });
 
         app.listen(process.env.PORT || 8181);
